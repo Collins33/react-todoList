5492bfd8a01923ce40b115f4ec3efc57f76ff570
Collins njau <collinsnjau39@gmail.com>
2018-02-15 19:32:16 +0300

    I have added the method the constructor to the class and bound the method that is called when you submit the form

--- src/components/todolist.js
+++ src/components/todolist.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
 class TodoList extends Component{
+
+    constructor(props,context){
+        super(props,context)
+        
+        //adding the class method
+        this.addItem=this.addItem.bind(this)
+    }
+
+    //the method called when you submit form
+
+    addItem(e){
+        
+    }
     render(){
         return(
             <div className="row">
-            <form className="form-inline">
+            <form className="form-inline" onSubmit={this.addItem}>
                 <div className="form-group">
                 <input className="form-control" placeholder="enter task"/>
                 </div>
